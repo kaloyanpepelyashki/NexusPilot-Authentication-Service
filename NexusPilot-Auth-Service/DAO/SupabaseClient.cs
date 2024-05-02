de98c4c10da6da0639ead231921570ddd756e122
kaloyanpepelyashki <kaloyan.pepelyashki@gmail.com>
2024-05-02 14:03:56 +0200

    Set-up the Authentication Service project of the NexusPilot Project Management App
    Set up the project scaffold of the authentication service. Installed some of the needed NuGet packages.

---
+++ NexusPilot-Auth-Service/DAO/SupabaseClient.cs
@@ -0,0 +1,25 @@
+﻿using Supabase;
+
+namespace NexusPilot_Auth_Service.DAO
+{
+    public class SupabaseClient
+    {
+        protected IConfiguration _configuration;
+        protected string SupabaseProjectUrl;
+        protected string SupabaseApiKey;
+        protected SupabaseOptions Options;
+        protected Client SupabaseAccessor;
+
+        public SupabaseClient()
+        {
+            _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true).AddEnvironmentVariables().Build();
+            SupabaseProjectUrl = _configuration["SupabaseConfig:ProjectUrl"];
+            SupabaseApiKey = _configuration["SupabaseConfig:ApiKey"];
+            Options = new Supabase.SupabaseOptions
+            {
+                AutoConnectRealtime = true
+            };
+            SupabaseAccessor = new Supabase.Client(SupabaseProjectUrl, SupabaseApiKey, Options);
+        }
+    }
+}
